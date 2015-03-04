5cc4c11caf50c0601368ac8015e7cc958900654c
41440927 <41440927@090012-P33S13.macklabs.br>
2015-03-04 09:30:28 -0300

    Terminado

--- ex41.php
+++ ex41.php
@@ -16,25 +16,25 @@ $proj4 = 8;
 $trabF = 4;
 $proc = 8;
 $pf = 6;
-$mi = (30*p1 + 10*m1 + 10*m2 + 5*proj1 + 5*proj2 + 5*proj3 + 5*proj4 + 10*trabF + 20*proc)/100;
+$mi = (30*$p1 + 10*$m1 + 10*$m2 + 5*$proj1 + 5*$proj2 + 5*$proj3 + 5*$proj4 + 10*$trabF + 20*$proc)/100;
 $mf = 0;
 $aprovadoDireto = false;
 
 if ($mi>7.5){
     $mf=$mi;
-    echo "Aprovado sem Prova Final";
+    echo "Aprovado sem Prova Final ";
     echo "Parabéns!! Boas Férias";
     $aprovadoDireto = true;
 }
 else{
     $mf=($mi+$pf)/2;
     if (($mf>=5) and ($aprovadoDireto == false)){
-        echo "Aprovado com Prova Final";
-        echo "Agora já pode descansar tranquilo";
+        echo "Aprovado com Prova Final ";
+        echo "Agora já pode descansar tranquilo ";
     }
     elseif ($mf<5){
-        echo "Reprovado";
-        echo "Não foi dessa vez. Ano que vem tem mais TWII.";
+        echo "Reprovado ";
+        echo "Não foi dessa vez. Ano que vem tem mais TWII. ";
         echo "Media Intermediária: ", $mi;
         echo "Media Final: ", $mf;
     }
