47487ec8e7e9c74d6ecece89f151c9dfb9c6fdd9
41440927 <41440927@090012-P33S13.macklabs.br>
2015-03-05 11:02:42 -0300

    Fibonacci terminado.

--- fibonacci.php
+++ fibonacci.php
@@ -3,18 +3,24 @@
     <head>
         <meta charset="UTF-8">
         <meta name="author" content="Otávio Roman">
-        <title>primo</title>
+        <title>fibonacci</title>
     </head>
     <body>
         <?php
-            $num=3;
-            $f=0;
+            $num=15;
+            $f=1;
             $resultado=0;
+            $resultado1=0;
+            $resultado2=0;
             for ($i=0;$i<$num;$i++){
-                $f=$resultado;
-                $resultado=$resultado+$f;
+                $resultado=$resultado1+$resultado2+$f;
+                $f=0;
+                $resultado2=$resultado1;
+                $resultado1=$resultado;
+                
+                echo $resultado.' ';
             }
-            echo $resultado;
+            
         ?>
         
     </body>
