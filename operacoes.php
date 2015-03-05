46d737f71cbb3668d3b029ec0df4538aee5f5dba
41440927 <41440927@090012-P33S13.macklabs.br>
2015-03-05 11:23:55 -0300

    Operações terminadas.

---
+++ operacoes.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset ="UTF-8">
+        <meta name="author" content="Otávio Roman">
+        <title>operacoes</title>
+    </head>
+    <body>
+        <?php
+        function soma_valores ($x, $y){
+            $soma=$x+$y;
+            return $soma;
+        }
+        function subtracao_valores ($x,$y){
+            $subtracao=$x-$y;
+            return $subtracao;
+        }
+        function divisao_valores ($x,$y){
+            $divisao=$x/$y;
+            return $divisao;
+        }
+        function multiplicacao_valores ($x,$y){
+            $multiplicacao=$x*$y;
+            return $multiplicacao;
+        }
+        ?>        
+    </body>
+</html>
